baffe450eb0c11698785a3c057841e11193d9f86
Angel <smlnatya@gmail.com>
2023-05-25 13:49:44 -0500

    Updating resolver and typeDefs

--- src/Pokemon/pokemonResolver.ts
+++ src/Pokemon/pokemonResolver.ts
@@ -1,9 +1,22 @@
 const pokemonResolver = {
-    pokemons: () => {
-        return [{
-            id: 123,
-            name: 'Gengar'
-        }]
+    pokemon: async (_parent, args, context) => {
+        const data = await context.dataSources.pokemonAPI.getPokemon(args.pokemonName);
+        return {
+            id: data.id,
+            name: data.name
+        };
+    },
+    ability: async (_parent, args, context) => {
+        const data = await context.dataSources.pokemonAPI.getAbility(args.abilityName);
+        return {
+            ability: data.name,
+            pokemons: data.pokemon.map(pokemon => (
+                {
+                    isHiddenAbility: pokemon.is_hidden,
+                    slot: pokemon.slot,
+                    name: pokemon.pokemon.name
+                }))
+        };
     }
 }
 
--- src/Pokemon/pokemonTypeDef.ts
+++ src/Pokemon/pokemonTypeDef.ts
@@ -1,10 +1,27 @@
 export const typeDefs = `#graphql
   type Query {
-    pokemons: [Pokemon]
+    pokemon(pokemonName: String!): Pokemon
+    ability(abilityName: String!): PokemonAbility
   }
 
   type Pokemon {
     id: ID
     name: String
   }
+
+  type PokemonAbility {
+    "The ability the Pokémon may have"
+    ability: String
+    "pokemons with this ability"
+    pokemons: [AbilityPokemon]
+
+  }
+
+  type AbilityPokemon {
+    isHiddenAbility: Boolean
+    "The slot this ability occupies in this Pokémon species" 
+    slot: Int
+    name: String
+}
+
 `
